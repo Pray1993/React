74fef1605042f1168fa02a254e2200be82452aa9
Pray1993 <117940770+Pray1993@users.noreply.github.com>
2022-11-11 01:01:18 +0530

    Functional and Class Components
    Functional and Class Components Creation

---
+++ Tutorials/hello-world/src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Praveen</h1>
+// }
+
+// writing Greet function with ES6 arrow function syntax
+const Greet = () => <h1>Hello Praveen</h1>
+
+export default Greet
